54ecede3062e28bde4118d99cc0b3f17a36c8cca
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-30 17:32:02 +0200

    feat: first part footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,65 @@
+function Footer() {
+    return (
+        <footer>
+            <section className="sectionFooter1 font">
+                <div className="menuFooter container flex">
+                    <div>
+                        <ul className="m-bt">
+                            <li className="m-bt"><h3>DC COMICS</h3></li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>Tv</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <ul>
+                            <li className="m-bt"><h3>SHOP</h3></li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className="m-bt"><h3>DC</h3></li>
+                            <li>Terms of Use</li>
+                            <li>Privacy policy(New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshop</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><h3>SITES</h3></li>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+
+                    <div className="logoBG">
+                        <img src="/dc-logo-bg.png" alt="" />
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="sectionFooter2">
+
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
