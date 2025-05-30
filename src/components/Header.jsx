dbe11b670e2b2ae230b8bdd51ab69a916a70ef18
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-30 15:23:01 +0200

    feat: header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+function Header() {
+    return (
+    <header className="flex">
+
+        <img src="/dc-logo.png" alt="" />
+        <nav>
+            <ul className="flex font menu">
+                <li><a href="">Characters</a></li>
+                <li><a href="">Comics</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">Tv</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Collectibles</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Fans</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Shop</a></li>
+                <li><a href="">Shop</a></li>
+            </ul>
+        </nav>
+        
+    </header>
+    )
+}
+
+export default Header
