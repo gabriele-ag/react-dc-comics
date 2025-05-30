8e42982a7ea3a755704cebcbe55be439788873b8
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-30 16:28:32 +0200

    feat: main done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,46 @@
 function Main() {
     return (
         <main>
-            
+
+            {/* Content*/}
+
+            <section className="sectionContent font">
+                <div className="container">
+                    <h1>Content goes here</h1>
+                </div>
+            </section>
+
+            {/* /Content*/}
+
+            {/* Menu-2 */}
+            <section className="sectionMenu2 font">
+                <div className="menu2 container flex">
+                    <div className="fontMenu flex">
+                        <img src="/buy-comics-digital-comics.png" alt="" />
+                        <a href="">DIGITAL COMICS</a>
+                    </div>
+                    <div className="fontMenu flex">
+                        <img src="/buy-comics-merchandise.png" alt="" />
+                        <a href="">DC MERCHANDISE</a>
+                    </div>
+                    <div className="fontMenu flex">
+                        <img src="/buy-comics-subscriptions.png" alt="" />
+                        <a href="">SUBSCRIPTION</a>
+                    </div>
+                    <div className="fontMenu flex">
+                        <img src="/buy-comics-shop-locator.png" alt="" />
+                        <a href="">COMIC SHOP LOCATOR</a>
+                    </div>
+                    <div className="fontMenu flex">
+                        <img src="/buy-dc-power-visa.svg" alt="" />
+                        <a href="">DC POWER VISA</a>
+                    </div>
+                    
+                </div>
+            </section>
+            {/* /Menu-2 */}
         </main>
     )
 }
+
+export default Main
